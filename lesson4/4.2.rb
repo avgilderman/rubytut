d5bab5ec240d0cdf3fdf7f8b911610cb70f674ea
AVG <46447533+avgilderman@users.noreply.github.com>
2022-10-03 21:59:24 +0600

    4.2

---
+++ lesson4/4.2.rb
@@ -0,0 +1,8 @@
+number1 = 2345654
+number2 = 3325654
+number3 = nil
+puts "Первое число: " + number1.to_s
+puts "Второе число: " + number2.to_s
+puts "Среднее: " + number3.to_s
+average = (number1 + number2) / 2
+average = number3
