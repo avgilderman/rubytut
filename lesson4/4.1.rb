fc3a0bbe6b93c8c8d92c42b2b86b196e3c6bc065
AVG <46447533+avgilderman@users.noreply.github.com>
2022-10-03 21:19:02 +0600

    начал новое задание 4.1

---
+++ lesson4/4.1.rb
@@ -0,0 +1,12 @@
+number1 = 198
+number2 = 474
+if number1 == number2
+	puts "число number1 и number2 равны"
+else
+if number1 > number2 
+	puts "Наибольшее число:" + number1.to_s
+else 
+	puts "Наибольшее число:" + number2.to_s
+end
+end
+
