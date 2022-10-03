673bafa65941bd6112d0f8a801264f713c5fc96b
AVG <46447533+avgilderman@users.noreply.github.com>
2022-10-03 22:18:34 +0600

    4.2

--- lesson4/4.2.rb
+++ lesson4/4.2.rb
@@ -1,7 +1,8 @@
 number1 = 2345654
 number2 = 3325654
+average = (number1 + number2) / 2
 number3 = average
 puts "Первое число: " + number1.to_s
 puts "Второе число: " + number2.to_s
 puts "Среднее: " + number3.to_s
-average = (number1 + number2) / 2
+
