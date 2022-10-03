4007d72b33be766e3df8a0ef5a8936c43cce3806
avgilderman <avgilderman@icloud.com>
2022-10-03 22:02:03 +0600

    4.2

--- lesson4/4.2.rb
+++ lesson4/4.2.rb
@@ -1,8 +1,7 @@
 number1 = 2345654
 number2 = 3325654
-number3 = nil
+number3 = average
 puts "Первое число: " + number1.to_s
 puts "Второе число: " + number2.to_s
 puts "Среднее: " + number3.to_s
 average = (number1 + number2) / 2
-number3 = average
