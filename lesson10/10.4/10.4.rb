38375e4bd7ec63cbd3d943d886cbf593f3b547b4
AVG <46447533+avgilderman@users.noreply.github.com>
2022-10-21 18:24:42 +0300

    windows shit

--- lesson10/10.4/10.4.rb
+++ lesson10/10.4/10.4.rb
@@ -1,42 +1,37 @@
 list_files = []
-array_file = []
-array_questions = []
-three_questions = []
+array_q_a = []
+number_of_questions = 3
 count_right_answers = 0
-
 puts "Мини-викторина. Ответьте на вопросы:"
 
-#делаем массив с именами файлов
-list_files = Dir.glob("data/*")
-current_path = File.dirname(__FILE__)
+#создаем список файлов в директории и записываем список в массив list_files
+list_files = Dir[File.join(__dir__, 'data', "*.txt")]
 
-list_files.each.with_index do |file, index|
-  #получаем путь до файлов
-  file_path = current_path + "/#{file}"
-  #присоединям файл к массиву файлов
-  array_file = File.new(file_path, "r:UTF-8")
-  #добавляем построчно новый файл в построчный массив
-  array_questions.push(array_file.readlines)
-  #закрываем файл
-  array_file.close
-end
+#Oбрабатываем элементы массива list_files прочитывая по порядку его содержимое
+#методом readlines, и записывая обработанные элементы в массив array_q_a
+array_q_a =
+  list_files.map do |file|
+    File.readlines(file, chomp: true, encoding: 'UTF-8')
+  end
 
 #кладем три случайных вопроса в отдельный массив
-three_questions = array_questions.sample(3)
+three_questions = array_q_a.sample(number_of_questions)
 
-#обрабатываем вопросы и ответы
-three_questions.each do |question|
-#вывести первую строчку из файла
-puts question[0]
-puts "Введите ответ:"
-user_answer = STDIN.gets.chomp.to_i
-if user_answer == question[1].to_i
+#обрабатываем вопросы и ответы. Заранее делим массив на вопрос и ответ
+three_questions.each.with_index(1) do |(question, right_answer), index|
+  #вывести первую строчку из файла
+  puts "#{index}. #{question}"
+  puts "Введите ответ:"
+user_answer = $stdin.gets.chomp
+if user_answer.downcase == right_answer.downcase
   puts "Вы правильно ответили!"
   puts
-count_right_answers += 1
+
+  count_right_answers += 1
+
 else
-  puts "Вы ошиблись, правильный ответ - #{question[1]}"
+  puts "Вы ошиблись, правильный ответ - #{right_answer}"
   puts
 end
 end
-puts "Правильных ответов: #{count_right_answers} из 3"
+puts "Правильных ответов: #{count_right_answers} из #{number_of_questions}"
