e91189d079633c0b0ccfdee6a1f8cf1bf4f7cace
AVG <avgilderman@icloud.com>
2022-12-06 16:05:27 +0300

    12.2 done

--- lesson12/12.2/12.2.rb
+++ lesson12/12.2/12.2.rb
@@ -1,23 +1,32 @@
-#Выводим сообщение приветственное
 puts "Введите слово, а я посчитаю гласные и согласные буквы:"
-
 #записываем в переменную ввод пользователя
-user_word = $stdin.gets.chomp.downcase
-
-#обьявляем метод который
-def letters(letter) #объявляем метод и его параметр
-  array = [] #обьявляем пустой массив
+user_word = $stdin.gets.chomp.downcase #Записываем ввод пользователя
+def letters(letter)
+  array = []
   array_gl = %w[а у о ы и э я ю ё е] #обьявляем массив с гласными буквами
-  count = 0 #объявляем переменную со счетчиком
-  array = letter.chars #
+  count = 0
+  array = letter.chars #Возвращает массив, содержащий символы строки, из которой вызывается
     array.each do |item|
-      if array_gl.include?(item)
-      count += 1
+      if array_gl.include?(item) #если элемент из массива введенного слова имеется в массие гласных, то
+      count += 1 #добавляем в переменную счетчика +1
       end
     end
-  other_letters = array.size - count
   return count
-  return other_letters
 end
 
-puts "Гласных буков: #{letters(user_word)}"
+def letters2(letter)
+  array = []
+  array_sg = %w[б в г д ж з й к л м н п р с т ф х ц ч ш щ]
+  count = 0
+  array = letter.chars
+    array.each do |item|
+      if array_sg.include?(item)
+        count += 1
+      end
+    end
+    return count
+  end
+
+puts "Гласных букв: #{letters(user_word)}"
+puts "Согласных букв: #{letters2(user_word)}"
+puts "Всего букв букв: #{letters(user_word) + letters2(user_word)}"
