5196bfaecbfa660c2ba11a9605821fe7aaa037a0
AVG <avgilderman@icloud.com>
2022-12-18 12:43:21 +0300

    2

--- lesson12/12.3/12.3.rb
+++ lesson12/12.3/12.3.rb
@@ -1,18 +1,24 @@
 puts "Какой сейчас курс доллара?"
-usd_rate = gets.chomp.to_f
+currency = gets.chomp.to_f
 puts "Сколько у вас рублей?"
-rub = gets.chomp.to_f
+how_many_rub = gets.chomp.to_f
 puts "Сколько у вас долларов?"
-usd = gets.chomp.to_f
+how_many_usd = gets.chomp.to_f
 
-def ()
-usdrub = (rub / usd_rate)
-diff = (usdrub - usd).abs
-average_diff = (diff / 2).round(2)
+def difference_usdrur (currency, how_many_usd, how_many_rub)
+  rub_to_usd = (rub / usd_rate)
+  diff = (rub_to_usd - usd).abs
+  average_diff = (diff / 2).round(2)
+    return (rub_to_usd, diff, average_diff)
+end
+
+def buy_or_sell (diff, usd, rub_to_usd, average_diff)
 if diff <= 0.01
   puts "Ваш портфель сбалансирован"
-elsif usd > usdrub
+elsif usd > rub_to_usd
   puts "Вам следует продать " + average_diff.to_s + " $"
 else
   puts "Вам следует купить " + average_diff.to_s + " $"
+end
+
 end
