0b13f8ded4d29e26f488fc8bb807a67ce5aee9f3
AVG <avgilderman@icloud.com>
2023-01-07 17:40:41 +0300

    12.3 is done 3

--- lesson12/12.3/12.3.rb
+++ lesson12/12.3/12.3.rb
@@ -1,30 +1,27 @@
+#Вычислаяем сколько у нас "долларов в рублях""
+def rub_to_usd(rate, how_many_rub)
+  rub_to_usd = how_many_rub / rate
+end
+#Подсчитываем разницу между "долларами в рублях" и долларами
+def diff(rub_to_usd, how_many_usd)
+  (rub_to_usd - how_many_usd).abs
+end
+#Считаем сколько нужно купить/продать долларов
+def how_many_buy(diff)
+  (diff / 2).round(2)
+end
+#Опрашиваем пользователя
 puts "Какой сейчас курс доллара?"
-rate = gets.chomp.to_f
+rate = gets.to_f
 puts "Сколько у вас рублей?"
-how_many_rub = gets.chomp.to_f
+how_many_rub = gets.to_f
 puts "Сколько у вас долларов?"
-how_many_usd = gets.chomp.to_f
-
-def diff_usdrur(rate, how_many_rub, how_many_usd)
-  rub_to_usd = (how_many_rub / rate)
-  diff = (rub_to_usd - how_many_usd).abs
-  how_many_buy = (diff / 2).round(2)
-  return rub_to_usd, diff, how_many_buy
+how_many_usd = gets.to_f
+#Решаем, что посоветовать  пользователю
+if diff(rub_to_usd(rate, how_many_rub), how_many_usd)<= 0.01
+  puts "Ваш портфель сбалансирован"
+elsif how_many_usd > rub_to_usd(rate, how_many_rub)
+  puts "Вам следует продать " + how_many_buy(diff(rub_to_usd(rate, how_many_rub), how_many_usd)).to_s + "$"
+else
+  puts "Вам следует купить " + how_many_buy(diff(rub_to_usd(rate, how_many_rub), how_many_usd)).to_s + "$"
 end
-
-def buy_or_sell(how_many_usd, rub_to_usd, diff, how_many_buy)
-  if diff <= 0.01
-    print_string = "Ваш портфель сбалансирован"
-  elsif how_many_usd > rub_to_usd
-    print_string = "Вам следует продать " + how_many_buy.to_s + " $"
-  else
-    print_string = "Вам следует купить " + how_many_buy.to_s + " $"
-  end
-  return print_string
-end
-
-rub_to_usd = diff_usdrur(rate, how_many_rub, how_many_usd)[0]
-diff = diff_usdrur(rate, how_many_rub, how_many_usd)[1]
-how_many_buy = diff_usdrur(rate, how_many_rub, how_many_usd)[2]
-
-puts buy_or_sell(how_many_usd, rub_to_usd, diff, how_many_buy)
