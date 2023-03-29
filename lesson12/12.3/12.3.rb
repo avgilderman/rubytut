8d51c8249b9b8decf8bf01b3a4b75ec7c7bd65c6
AVG <avgilderman@icloud.com>
2023-03-29 17:27:42 +0300

    еуые

--- lesson12/12.3/12.3.rb
+++ lesson12/12.3/12.3.rb
@@ -1,27 +1,31 @@
-#Вычислаяем сколько у нас "долларов в рублях""
+#расчет сколько у нас будет долларов, если поменяем свои рубли.
 def rub_to_usd(rate, how_many_rub)
-  rub_to_usd = how_many_rub / rate
+    how_many_rub / rate
 end
-#Подсчитываем разницу между "долларами в рублях" и долларами
-def diff(rub_to_usd, how_many_usd)
-  (rub_to_usd - how_many_usd).abs
+#подсчитывает разницу между долларами которые у нас будут после обмена и долларами, что мы уже имеем.
+def diff(rate, how_many_rub)
+    rub_to_usd = rub_to_usd(rate, how_many_rub)
+    (how_many_usd - rub_to_usd).abs
 end
-#Считаем сколько нужно купить/продать долларов
+#Считаем сколько нужно купить/продать долларов, что бы выровнять портфель
 def how_many_buy(diff)
-  (diff / 2).round(2)
+    (diff / 2).round(2)
 end
+#Решаем, что посоветовать  пользователю
+if diff(rate, how_many_rub)<= 0.01
+  puts "Ваш портфель сбалансирован"
+elsif how_many_usd > rub_to_usd(rate, how_many_rub)
+  puts "Вам следует продать " + how_many_buy.to_s + "$"
+else
+  puts "Вам следует купить " + how_many_buy.to_s + "$"
+end
+
 #Опрашиваем пользователя
-puts "Какой сейчас курс доллара?"
+puts "Какой сейчас курс доллара к рублю?"
 rate = gets.to_f
 puts "Сколько у вас рублей?"
 how_many_rub = gets.to_f
 puts "Сколько у вас долларов?"
 how_many_usd = gets.to_f
-#Решаем, что посоветовать  пользователю
-if diff(rub_to_usd(rate, how_many_rub), how_many_usd)<= 0.01
-  puts "Ваш портфель сбалансирован"
-elsif how_many_usd > rub_to_usd(rate, how_many_rub)
-  puts "Вам следует продать " + how_many_buy(diff(rub_to_usd(rate, how_many_rub), how_many_usd)).to_s + "$"
-else
-  puts "Вам следует купить " + how_many_buy(diff(rub_to_usd(rate, how_many_rub), how_many_usd)).to_s + "$"
-end
+
+
