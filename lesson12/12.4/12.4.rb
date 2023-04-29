fc29aa71427a8e81824df22a8627e1ea25aa24c9
AVG <46447533+avgilderman@users.noreply.github.com>
2023-04-29 20:04:18 +0600

    12.4 is done

--- lesson12/12.4/12.4.rb
+++ lesson12/12.4/12.4.rb
@@ -1,50 +1,33 @@
 def BMI(height, weight)
   height_meter = height / 100
-  weight / (height_meter * height_meter)
+  (weight / (height_meter * height_meter)).round(1)
 end
 
-puts BMI()
+def index_bmi_string(index)
+  index = index
+  case index
+    when 0..16
+      puts "Ваш индекс: #{index} - Выраженный дефицит массы тела" 
+    when 16..18,5
+      puts "Ваш индекс: #{index} - Недостаточная (дефицит) масса тела"
+    when 18,5..25
+      puts "Ваш индекс: #{index} - Норма"
+    when 25..30
+      puts "Ваш индекс: #{index} - Избыточная масса тела (предожирение)"
+    when 30..35
+      puts "Ваш индекс: #{index} - Ожирение 1 степени"
+    when 35..40
+      puts "Ваш индекс: #{index} - Ожирение 2 степени"
+    else
+      puts "Ваш индекс: #{index} - Ожирение 3 степени"
+  end
+end
 
-puts "Какой у Вас рост?"
+puts "Введите ваш рост (в cм.):"
 height = gets.to_f
-puts "Какой у Вас вес?"
+puts "Введите ваш вес (в кг.):"
 weight = gets.to_f
-puts "Ваш индекс массы тела: "
 
+index = BMI(height, weight)
 
-# Вычисляется этот индекс по простой формуле — масса в килограммах, деленная на квадрат 
-# роста в метрах.
-
-# Недостаточная (дефицит) масса тела 
-
-# первый по параметрам вычисляет BMI;
-# второй по показателю BMI определяет строку для вывода по таблице из Википедии.
-# Для второго подойдет конструкция case.
-
-# def rub_to_usd(rate, how_many_rub)
-#     how_many_rub / rate
-# end
-  
-#   def diff(rate, how_many_rub, how_many_usd)
-#     (how_many_usd - rub_to_usd(rate, how_many_rub)).abs
-#   end
-  
-#   def how_many_buy(rate, how_many_rub, how_many_usd)
-#     (diff(rate, how_many_rub, how_many_usd) / 2).round(2)
-#   end
-  
-#   puts "Какой сейчас курс доллара к рублю?"
-#   rate = gets.to_f
-#   puts "Сколько у вас рублей?"
-#   how_many_rub = gets.to_f
-#   puts "Сколько у вас долларов?"
-#   how_many_usd = gets.to_f
-  
-#   if diff(rate, how_many_rub, how_many_usd) <= 0.01
-#     puts "Ваш портфель сбалансирован"
-#   elsif how_many_usd > rub_to_usd(rate, how_many_rub)
-#     puts "Вам следует продать " + how_many_buy(rate, how_many_rub, how_many_usd).to_s + "$"
-#   else
-#     puts "Вам следует купить " + how_many_buy(rate, how_many_rub, how_many_usd).to_s + "$"
-#   end
-  
+puts index_bmi_string(index)
