f90b85d92dcda3b43eee12a5a519f312686b4b9d
AVG <46447533+avgilderman@users.noreply.github.com>
2023-05-20 20:21:15 +0600

    13.3 is done

--- lesson13/13.2/13.2.rb
+++ lesson13/13.2/13.2.rb
@@ -9,16 +9,40 @@ class Address
   end
 
   def full_address
-    return "#{@district} #{@area} #{@city} #{@street} #{@house} #{@flat}"
-  end
+    array = []
+    
+    if !@street.to_s.empty?
+      array << "Улица #{@street}"
+    end
+    
+    if !@house.to_s.empty?
+      array << @house
+    end
+    
+    if !@flat.to_s.empty?
+      array << "квартира #{@flat}"
+    end
+    
+    if !@city.to_s.empty?
+      array << "гор. #{@city}"
+    end
+    
+    if !@area.to_s.empty?
+      array << "#{@area} район"
+    end
+    
+    if !@district.to_s.empty?
+      array << "#{@district} обл."
+    end
 
+    return array.join(", ")
+  end
 end
 
-address_1 = Address.new("Московская область", "Орехово-Зуевский район", 
-  "Орехово-Зуево", "Первый колхозный проезд", "Дом", "3")
-address_2 = Address.new("Московская область", "Орехово-Зуевский район", 
-  "Орехово-Зуево", "Гагарина", "23А", "12")
+address_1 = Address.new("Московская", nil, "Орехово-Зуево", "Первый колхозный проезд", 
+  "Дом", "3")
+address_2 = Address.new("Московская область", "Орехово-Зуевский район", "Орехово-Зуево", "Гагарина", "23А", "12")
 
-puts "1 адрес #{address_1.full_address}"
+puts "Первый адрес: #{address_1.full_address}"
 puts
-puts "2 адрес #{address_2.full_address}"
+puts "Второй адрес: #{address_2.full_address}"
