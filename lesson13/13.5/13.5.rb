445a7ab9ed661d0faadf8d9c8f8bcf0e4fca8aa8
AVG <46447533+avgilderman@users.noreply.github.com>
2023-08-04 20:51:30 +0600

    13.5 is done

--- lesson13/13.5/13.5.rb
+++ lesson13/13.5/13.5.rb
@@ -1,63 +1,43 @@
 require_relative 'lib/question'
 
-list_files = []
+#объявляем все, что нам понадобиться:
 array_q_a = []
 array_questions = []
 count_right_answers = 0
 count_scores = 0
+user_answer = nil
 
 #поиск файлов, соответствующих шаблону, и создание списка таких файлов
 list_files = Dir[File.join(__dir__, 'data', "*.txt")]
 
-#
-array_q_a =
+#Итератор применяет к списку адресов файлов - метод фаилз с параметрами и добавляет значения в массив вопросов и ответов. Затем итератор проходит по массиву вопросов и ответов, добавляя строчки с вопросами/ответами/баллами в конструктор класса и затем добавляет создавшиеся обьекты класса в массив
+
 list_files.map do |file|
-  File.readlines(file, chomp: true, encoding: 'UTF-8')
+  array_q_a = File.readlines(file, chomp: true, encoding: 'UTF-8')
+  array_questions << Question.new(array_q_a[0], array_q_a[1], array_q_a[2])
 end
 
-array_q_a.each do |value|
-  array_questions << Question.new(value[0], value[1], value[2])
+puts "Мини-викторина. Ответьте на вопросы:"
+puts 
+
+5.times do |index|
+  shuffle_questions = array_questions.sample
+  array_questions.delete(shuffle_questions)
+  puts "Вопрос #{index + 1}. #{shuffle_questions.question}"
+  puts "Введите ответ:"
+  user_answer = $stdin.gets.chomp
+  #проверка ответа
+ if user_answer.downcase == shuffle_questions.right_answer.downcase
+    puts "Вы правильно ответили!"
+    puts
+    count_scores += shuffle_questions.scores.to_i
+    count_right_answers += 1
+  else
+    puts "Вы ошиблись, правильный ответ - #{shuffle_questions.right_answer}"
+    puts
+  end
 end
 
+puts "Правильных ответов: #{count_right_answers} из 5"
+puts "Вы набрали #{count_scores} баллов"
 
-puts array_questions[3].right_answer
-
-
-
-#class MyClass
- # def initialize(value)
-  #  @value = value
-  #end
-#end
-
-#values = [1, 2, 3, 4, 5]
-#objects = []
-
-#values.each do |value|
- # objects << MyClass.new(value)
-#end
-
-#five_questions = array_q_a.sample(5)
-
-#puts "Мини-викторина. Ответьте на вопросы:"
-
-#five_questions.each.with_index(1) do |(questinon, right_answer, scores), index|
-
-#  puts "#{index}. #{questinon}"
- # puts "Введите ответ:"
-  #user_answer = $stdin.gets.chomp
-  
-  #if user_answer.downcase == right_answer.downcase
-   # puts "Вы правильно ответили!"
-    #puts
-    #count_scores += scores.to_i
-    #count_right_answers += 1
-  #else
-  #  puts "Вы ошиблись, правильный ответ - #{right_answer}"
-   # puts
-  #end
-
-#end
-
-#puts "Правильных ответов: #{count_right_answers} из 5"
-#puts "Вы набрали #{count_scores} баллов"
