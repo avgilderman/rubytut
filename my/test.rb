3f407af307485d4ef023928e8f441d1feeb95639
AVG <46447533+avgilderman@users.noreply.github.com>
2023-12-12 22:23:05 +0600

    Пробуем

--- my/test.rb
+++ my/test.rb
@@ -1,5 +1,5 @@
 def test_format(format_string)
-  puts "Testing #{format_string} : #{format(format_string, 12.3456)}"
+  puts "Testing #{format_string} : #{format(fo rmat_string, 12.3456)}"
 end
 
 test_format "%.2f"
