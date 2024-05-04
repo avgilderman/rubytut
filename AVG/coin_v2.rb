91c03cbb7605845a959eb60275f9617faef5db48
AVG <46447533+avgilderman@users.noreply.github.com>
2024-05-04 11:10:01 +0500

    test 4

---
+++ AVG/coin_v2.rb
@@ -0,0 +1,9 @@
+puts "Подбросим монетку!"
+random = rand(1000)
+if random == 1
+  puts "Монетка упала третьей стороной"
+elsif random.even?
+  puts "Выпал орел"
+elsif random.odd?
+  puts "Выпал решка"
+end
