11f1925555ce70ded456e801d9e4d5f6b76636cc
Alex Bork <avg@AVG-AIR.local>
2024-06-20 19:10:39 +0500

    155 is done

--- lesson15/150/main.rb
+++ lesson15/150/main.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-# изменение кодировки, если windows
 if Gem.win_platform?
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
@@ -10,28 +7,26 @@ if Gem.win_platform?
   end
 end
 
-# подключаем классы
-require_relative 'lib/console_interface'
-require_relative 'lib/game'
+require_relative "lib/console_interface"
+require_relative "lib/game"
 
-# приветствие
-puts 'Всем привет!'
+# 1. Поздороваться
+puts "Всем привет!"
 
-# записываем загаданное слово в переменную
-word = File.readlines("#{__dir__}/data/words.txt", encoding: 'UTF-8', chomp: true).sample
-
-# создаем экземпляр класса game с параметром в виде загаданного слова
+# 2. Загрузить случайное слово из файла
+word = File.readlines("#{__dir__}/data/words.txt", encoding: "UTF-8", chomp: true).sample
 game = Game.new(word)
-
-# создаем экземпляр класса interface c параметром в виде экземплярка класса game
-
 console_interface = ConsoleInterface.new(game)
 
-# цикл самой игры
-until game.over? # проверяем не закончилась ли игра?
-  console_interface.print_out # выводит интерфейс игры
-  letter = console_interface.get_input # обработка введеной пользователем буквы
-  game.play!(letter) # записываем ввередную букву в массив введеных пользователем букв
+# 3. Пока не закончилась игра
+until game.over?
+  #   3.1. Вывести очередное состояние игры
+  console_interface.print_out
+  #   3.2. Спросить очередную букву
+  letter = console_interface.get_input
+  #   3.3. Обновить состояние игры
+  game.play!(letter)
 end
 
-console_interface.print_out # вывести финальное состояние игры
+# 4. Вывести финальное состояние игры
+console_interface.print_out
---
+++ lesson15/155/lib/console_interface.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# класс отвечающий за интерфейс
+class ConsoleInterface
+  FIGURES =
+    Dir["#{__dir__}/../data/figures/*.txt"]
+    .sort
+    .map { |file_name| File.read(file_name) } # прочитать, отсортировать, прочитать каждый файл и засунуть все в константу массива FIGURES # rubocop:disable Layout/LineLength
+
+  # экземпляр класса game с загаданным словом в качестве параметра класса consoleinterface
+  def initialize(game)
+    @game = game
+  end
+
+  # вывести интерфейс игры(слово с угаданными буквами и пробелы в нем, рисунок виселицы, количество ошибок и сами ошибочные буквы, и количество оставшихся попыток) # rubocop:disable Layout/LineLength
+  def print_out
+    puts <<~HEREDOC
+      Слово: #{word_to_show}
+      #{figure}
+      Ошибки (#{@game.errors_made}): #{errors_to_show}
+      У вас осталось ошибок: #{@game.errors_allowed}
+    HEREDOC
+
+    if @game.won? # сообщение выходящее по завершении игры
+      puts 'Поздравляем, вы выиграли!'
+    elsif @game.lost?
+      puts "Вы проиграли, загаданное слово: #{@game.word}"
+    end
+  end
+
+  # возвращает рисунок равный количеству ошибок пользователя
+  def figure
+    FIGURES[@game.errors_made]
+  end
+
+  # заменить nil на "__"
+  def word_to_show
+    result =
+      @game.letters_to_guess.map do |letter|
+        if letter.nil?
+          '__'
+        else
+          letter
+        end
+      end
+
+    result.join(' ') # обьединить элементы массива в строку
+  end
+
+  def errors_to_show
+    @game.errors.join(', ') # преобразовать массив ошибочных букв в строку
+  end
+
+  # просьба ввести следующую букву
+  def get_input
+    print 'Введите следующую букву: '
+    letter = gets[0].upcase
+  end
+end
