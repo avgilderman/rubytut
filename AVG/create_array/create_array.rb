f20ded44b03817b1560f4e1a4852451ccca887e1
AVG <46447533+avgilderman@users.noreply.github.com>
2024-12-16 12:53:04 +0500

    create_array is done 2

--- AVG/create_array/create_array.rb
+++ AVG/create_array/create_array.rb
@@ -3,10 +3,10 @@
 require 'fileutils'
 require_relative 'lib/methods'
 
-file_path = 'data/data.txt'
-folder_name = 'archive'
+data_file_path = 'data/data.txt'
+output_folder = 'archive'
 
-full_file_path = file_path(file_path) # есть ли входной файл? Узнать полный путь файла
-file_name = clear_file_name(file_path) # узнать имя входного файла
-create_directory(folder_name) # создаем директорию для выходных файлов
-create_files(full_file_path, folder_name, file_name) # создаём выходные файлы
+full_file_path = file_path(data_file_path) # есть ли входной файл? Узнать полный путь файла
+file_name = clear_file_name(data_file_path) # узнать имя входного файла
+create_directory(output_folder) # создаем директорию для выходных файлов
+create_files(full_file_path, output_folder, file_name) # создаём выходные файлы
