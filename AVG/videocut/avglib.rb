fdb3cf77d8cdc2bf3d3fc8221c66b4fcc2524121
AVG <46447533+avgilderman@users.noreply.github.com>
2025-02-19 11:40:20 +0500

    avglib v1

---
+++ AVG/videocut/avglib.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'fileutils' # нужен для create_folders
+require 'bundler' # нужен для запуска bundler в программе
+require 'bundler/cli' # нужен для запуска установки гемов из gemfile
+
+# Модуль с универсальными методами для программ v1
+module AVGlib
+  # метод для запуска main файла из любой директории локальной ОС
+  def self.rootdir
+    root_dir = File.dirname(File.expand_path(__FILE__))
+    Dir.chdir(root_dir)
+    root_dir
+  end
+
+  # метод для создания папок (принимает массив переменных с путями)
+  def self.create_folders(folders)
+    folders.each do |folder|
+      FileUtils.mkdir_p(folder)
+    rescue StandardError => e
+      puts "Ошибка создания папки '#{folder}': #{e.message}"
+    end
+  end
+
+  # метод для автоматической установки гемов из bundle непосредственно во время первого исполнения программы
+  def self.install_gemfile
+    gemfile_path = File.join(rootdir, 'Gemfile')
+    if File.exist?(gemfile_path) # если gemfile существует, то
+      Bundler::CLI.start(['install']) unless File.exist?(File.join(rootdir, 'Gemfile.lock')) # если файла lock нет
+    else
+      puts 'Gemfile не найден в корневой директории проекта.'
+    end
+  end
+end
---
+++ AVG/videocut/methods.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+install_gemfile
+require 'fileutils'
+require 'streamio-ffmpeg'
+
+# внутреняя работа FFMPEG
+module FFMPEG
+  def self.ffmpeg_installed?
+    FFMPEG::Movie.new('').metadata # проверяем созданием объекта и вызовом метода работу FFMPEG
+    true
+  rescue StandardError
+    # Если возникает ошибка, выбрасываем исключение с сообщением
+    raise 'FFmpeg не найден. Установите его, следуя инструкциям на https://ffmpeg.org/download.html'
+  end
+
+  # Разделение видео на части
+  def self.split_video(file_path, output_folder, max_size_mb)
+    FileUtils.mkdir_p(output_folder)
+    base_name = File.basename(file_path, File.extname(file_path))
+    output_pattern = File.join(output_folder, "#{base_name}_part%03d.mp4")
+
+    command = [
+      'ffmpeg', '-i', file_path,
+      '-c', 'copy', '-map', '0',
+      '-f', 'segment', '-segment_size', (max_size_mb * 1024 * 1024).to_s, # что за шляпааа?
+      '-reset_timestamps', '1',
+      output_pattern
+    ]
+
+    system(*command)
+    puts "Разделение завершено: #{file_path}"
+  end
+end
+
+# Основная функция
+module Main
+  def self.run(source_folder, output_folder, max_size_mb)
+    FFMPEGHelper.check_ffmpeg_installed
+
+    unless Dir.exist?(source_folder)
+      puts "Папка #{source_folder} не найдена!"
+      return
+    end
+
+    FileUtils.mkdir_p(output_folder)
+
+    Dir.glob(File.join(source_folder, '*')).each do |file_path|
+      next unless File.file?(file_path)
+
+      file_output_folder = File.join(output_folder, File.basename(file_path, '.*'))
+      VideoProcessor.split_video(file_path, file_output_folder, max_size_mb)
+    end
+  end
+end
